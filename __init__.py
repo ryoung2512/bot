fd842ea8f875429f2441fb8b0a6eabb6c4232596
Rhys <rhysyoung16@gmail.com>
2017-03-21 00:18:49 -0400

    p

--- __init__.py
+++ __init__.py
@@ -9,8 +9,20 @@ def listPlugins():
         plugins.append(plugin[0])
     return plugins
 
+#make this a constant that way we have to reload to check for new plugins
+PLUGINS = listPlugins()
+
+def validateCommand(user_input):
+    split_input = user_input.split()
+    if split_input[0] in PLUGINS:
+        print("Valid command can now be executed")
+    else:
+        print("Sorry, that was an invalid command!")
+
 def main():
+    os.system('clear')
     user_input = ""
     while user_input != "quit":
         user_input = input(">> ")
-        print("You entered " + user_input)
+        validateCommand(user_input)
+main()
