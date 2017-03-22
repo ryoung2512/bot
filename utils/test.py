aeb5c8b3b62770219d837dad926a9d90eb45e7b1
Rhys <rhysyoung16@gmail.com>
2017-03-21 22:40:29 -0400

    p

--- utils/test.py
+++
@@ -1,9 +0,0 @@
-import urllib2, urllib, json
-baseurl = "https://query.yahooapis.com/v1/public/yql?"
-yql_query = "select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"guelph, on\") and u='c'"
-yql_url = baseurl + urllib.urlencode({'q':yql_query}) + "&format=json"
-result = urllib2.urlopen(yql_url).read()
-data = json.loads(result)
-results = data['query']['results']['channel']['item']['condition']
-print "Temperature is: " + results['temp']
-print "It is " + results['text'] + " outside"
